c626a04ab6863e99b5e4215c06eeb7b7af8f72c5
永田マリ <nagamari80@gmail.com>
2020-06-11 18:11:50 +0900

    create implement

---
+++ src/actions/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+
+export const increment = () => ({
+  type: INCREMENT,
+});
+
+export const decrement = () => ({
+  type: DECREMENT,
+});
