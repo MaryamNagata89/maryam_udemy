720a562cc4cca989ec2d7d70ce40b4290d7a7259
永田マリ <nagamari80@gmail.com>
2020-06-11 17:27:20 +0900

    create component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <>
-        <div>Hello World</div>
-      </>
-    );
-  }
-}
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onChange={() => {console.log("I am clicked!")}} />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const App = () => {
+  return (
+    <div>
+      <Cat />
+      <Cat />
+      <Cat />
+      <Cat />
+    </div>
+  );
+};
+
+const Cat = () => {
+  return <div>Meow!</div>;
+};
 
 export default App;
